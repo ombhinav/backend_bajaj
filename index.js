163d7911baeebb2f75d1310209c43a5fc21d2187
Abhinav Garg <83314128+ombhinav@users.noreply.github.com>
2024-08-25 14:56:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,19 +5,9 @@ const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.json());
-app.use(cors());
 
-const allowedOrigins = ['*'];
-app.use(cors({
-  origin: function(origin, callback){
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){
-      var msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  }
-}));
+
+app.use(cors());
 
 // POST route
 app.post('/bfhl', (req, res) => {
